db7552d656ab5db967eef3d79286e625a9bd549d
Austin-Landschoot <austin.landschoot@students.coderva.org>
2025-05-19 22:19:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,13 +31,13 @@ if not is_admin():
     sys.exit(1)
 
 print_info("Running TOR setup...")
-exit_code = os.system(f"{sys.executable} /setup_subscripts/TOR_Setup.py")
+exit_code = os.system(f"{sys.executable} setup_subscripts/TOR_Setup.py")
 if exit_code != 0:
     print_warning("TOR setup failed.")
     sys.exit(exit_code)
 
 print_info("Running Python setup...")
-exit_code = os.system(f"{sys.executable} /setup_subscripts/Python_Setup.py")
+exit_code = os.system(f"{sys.executable} setup_subscripts/Python_Setup.py")
 if exit_code != 0:
     print_warning("Python setup failed.")
     sys.exit(exit_code)
