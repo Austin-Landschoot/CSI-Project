838390124105ff09b99b75d3ced6c1aa018124d4
Austin-Landschoot <austin.landschoot@students.coderva.org>
2025-05-19 21:19:44 -0400

    Update TOR_Setup.py

--- setup_subscripts/TOR_Setup.py
+++ setup_subscripts/TOR_Setup.py
@@ -5,7 +5,8 @@ import shutil
 import subprocess
 import tarfile
 import time
-from indicators import print_warning, print_success, print_info, print_prompt
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from UI.indicators import print_warning, print_success, print_info, print_prompt
 
 # I am only familiar with apt so other tor install commands may not work.
 managers = {
