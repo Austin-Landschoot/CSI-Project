a12b0e28189984bc0c01a301f359387a031578a9
Austin-Landschoot <austin.landschoot@students.coderva.org>
2025-05-29 09:32:23 -0400

    Update RansomWare_Generator.py

--- payloads/RansomWare/RansomWare_Generator.py
+++ payloads/RansomWare/RansomWare_Generator.py
@@ -3,6 +3,8 @@ import base64
 import time
 from pathlib import Path
 from os.path import isfile
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from UI.indicators import print_info, print_prompt, print_warning, print_success
 
